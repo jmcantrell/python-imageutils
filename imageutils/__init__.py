96542b42e7b6c24a3d4d5d657453f08968e7236e
Jeremy Cantrell <jmcantrell@gmail.com>
2012-04-28 21:59:39 -0400

    add exif function

--- imageutils/__init__.py
+++ imageutils/__init__.py
@@ -1,14 +1,17 @@
 import os
 from PIL import Image
+from PIL.ExifTags import TAGS
 
 __all__ = ['color', 'size', 'compose']
 
-def find_images(directory): #{{{1
+
+def find_images(directory):
     """Finds all images in a directory (recursive)."""
     for root, dirs, files in os.walk(directory):
         for d in dirs:
             ld = os.path.join(root, d)
-            if not os.path.islink(ld): continue
+            if not os.path.islink(ld):
+                continue
             for img in find_images(ld):
                 yield img
         for f in files:
@@ -16,3 +19,13 @@ def find_images(directory): #{{{1
                 yield Image.open(os.path.join(root, f))
             except:
                 continue
+
+
+def get_exif(filename):
+    ret = {}
+    i = Image.open(filename)
+    info = i._getexif()
+    for tag, value in info.items():
+        decoded = TAGS.get(tag, tag)
+        ret[decoded] = value
+    return ret
